5b66eeb5cd94ef7b1fb2f895bfc4c25480976add
chetana9981 <101543143+chetana9981@users.noreply.github.com>
2022-11-28 18:09:40 +0530

    Add files via upload

---
+++ BookStoreCaseStudy/src/main/kotlin/com/axis/book/dbConnection/dbConnection.kt
@@ -0,0 +1,14 @@
+package com.axis.book.dbConnection
+import java.sql.Connection
+import java.sql.DriverManager
+
+class dbConnection {
+
+    fun connect(): Connection
+    {
+        val myurl ="jdbc:mysql://localhost:3306/bookstoredb"//localhost is the database name
+        val connection= DriverManager.getConnection(myurl,"root","Auto@12345#")
+        return connection
+    }
+
+}
